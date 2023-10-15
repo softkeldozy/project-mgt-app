cdb9c3a36410f936052e299f1140175a50f56804
Kelvin Obidozie <kelvinobidozie@programmer.net>
2023-10-15 12:24:56 +0100

    fetch and delete client functionality

--- client/src/components/clientRow/ClientRow.jsx
+++ client/src/components/clientRow/ClientRow.jsx
@@ -1,13 +1,28 @@
 import { FaTrash } from 'react-icons/fa';
+import { useMutation } from '@apollo/client';
+import { DELETE_CLIENT } from '../../mutations/clientMutations.js';
+import { GET_CLIENTS } from '../queries/clientQueries.js';
 
 const ClientRow = ({ client }) => {
+  const [deleteClient] = useMutation(DELETE_CLIENT, {
+    //* variables was passed in as a second parameter so we can pass in the id of the client about to be deleted
+    variables: { id: client.id },
+    //* fetching back the client data after delete so the table displays the current client details without making a whole new request.
+    update(cache, { data: { deleteClient } }) {
+      const { clients } = cache.readQuery({ query: GET_CLIENTS });
+      cache.writeQuery({
+        query: GET_CLIENTS,
+        data: { clients: clients.filter(client => client.id !== deleteClient.id) }
+      });
+    }
+  });
   return (
     <tr>
       <td>{client.name}</td>
       <td>{client.email}</td>
       <td>{client.phone}</td>
       <td>
-        <button className="btn btn-danger btn-sm">
+        <button className="btn btn-danger btn-sm" onClick={deleteClient}>
           <FaTrash />
         </button>
       </td>
