74f1a6e5621cefce13e93a3ae67245c13fc41b30
Kelvin Obidozie <kelvinobidozie@programmer.net>
2023-10-15 15:09:26 +0100

    Add Client functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import Header from "./components/header/Header";
 import Clients from "./components/clients/Clients";
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+import AddClientModal from "./components/addClientModel/AddClientModal";
 
 const cache = new InMemoryCache({
   typePolicies: {
@@ -33,6 +34,7 @@ function App() {
       <ApolloProvider client={client} >
         <Header />
         <div className="container">
+          <AddClientModal />
           <Clients />
         </div>
       </ApolloProvider>
